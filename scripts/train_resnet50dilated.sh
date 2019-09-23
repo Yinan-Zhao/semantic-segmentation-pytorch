3f22ed7f7eb229275346827c910189d101208a3f
Zhao <alexzhao@cs.utexas.edu>
2019-09-23 15:41:19 -0500

    Initial commit

---
+++ scripts/train_resnet50dilated.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export PATH=/scratch/cluster/alexzhao/anaconda3/bin:$PATH
+cd /scratch/cluster/alexzhao/semantic-segmentation-pytorch
+source activate seg_pytorch
+python train.py --gpus 0 --cfg config/ade20k-resnet50dilated-ppm_deepsup.yaml

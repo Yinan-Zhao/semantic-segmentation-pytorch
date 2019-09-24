ffde2e805d005a0562197b954af945626a8a2f64
Zhao <alexzhao@cs.utexas.edu>
2019-09-24 10:39:23 -0500

    Initial commit

---
+++ sense_scripts/train_resnet50dilated.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+srun --mpi=pmi2 -p $1 -n1 --gres=gpu:8 --ntasks-per-node=1 --cpus-per-task=5 \
+python train.py --gpus 0-7 --cfg config/ade20k-resnet50dilated-ppm_deepsup.yaml \
+2>&1 | tee ./output.log

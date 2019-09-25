b105f0fec826d2b69331276f533fbef39c690d98
Zhao <alexzhao@cs.utexas.edu>
2019-09-24 21:11:16 -0500

    Initial commit

--- sense_scripts/train_resnet18dilated-c1_batch1.sh
+++ sense_scripts/train_resnet18dilated-c1_batch1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 nvidia-smi
 srun --mpi=pmi2 -p $1 -n1 --gres=gpu:8 --ntasks-per-node=1 --cpus-per-task=5 \
-stdbuf -oL python -u train.py --gpus 0,1,2,3 --cfg config/ade20k-resnet18dilated-c1_batch1.yaml \
+python -u train.py --gpus 0,1,2,3 --cfg config/ade20k-resnet18dilated-c1_batch1.yaml \
 2>&1 | tee ./output_c1_batch1.log

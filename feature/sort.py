eba28970535a7017cf501d9be40802476a091249
Zhao <alexzhao@cs.utexas.edu>
2019-10-02 22:45:04 -0500

    Initial commit

--- feature/sort.py
+++ feature/sort.py
@@ -6,7 +6,23 @@ val_input = '/mnt/lustre/zhaoyinan/semantic-segmentation-pytorch/feature/feat_va
 feat_train = np.load(train_input)
 feat_val = np.load(val_input)
 
+print('computing distance matrix')
+dis_val = distance_matrix(feat_val, feat_train)
+print('done')
+with open('/mnt/lustre/zhaoyinan/semantic-segmentation-pytorch/data/ref_real_val.txt', 'w') as f:
+    for i in range(dis_val.shape[0]):
+        print(i)
+        dis = dis_val[i]
+        order = np.argsort(dis)
+        for k, item in enumerate(order):
+            if k != len(order)-1:
+                f.write('%d '%(item))
+            else:
+                f.write('%d\n'%(item))
+
+'''print('computing distance matrix')
 dis_train = distance_matrix(feat_train, feat_train)
+print('done')
 with open('/mnt/lustre/zhaoyinan/semantic-segmentation-pytorch/data/ref_real_training.txt', 'w') as f:
     for i in range(dis_train.shape[0]):
         print(i)
@@ -16,7 +32,7 @@ with open('/mnt/lustre/zhaoyinan/semantic-segmentation-pytorch/data/ref_real_tra
             if k != len(order)-1:
                 f.write('%d '%(item))
             else:
-                f.write('%d\n'%(item))
+                f.write('%d\n'%(item))'''
 
 
 

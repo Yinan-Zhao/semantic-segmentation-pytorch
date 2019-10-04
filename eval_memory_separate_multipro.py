2c9948b1ea966bc3867360041f1fa3b231a671fd
Zhao <alexzhao@cs.utexas.edu>
2019-10-04 13:07:46 -0500

    Initial commit

--- eval_memory_separate_multipro.py
+++ eval_memory_separate_multipro.py
@@ -128,7 +128,7 @@ def worker(cfg, gpu_id, start_idx, end_idx, result_queue):
 
     crit = nn.NLLLoss(ignore_index=-1)
 
-    segmentation_module = SegmentationModule(net_enc_query, net_enc_memory, net_att_query, net_att_memory, net_decoder, crit)
+    segmentation_module = SegmentationAttentionSeparateModule(net_enc_query, net_enc_memory, net_att_query, net_att_memory, net_decoder, crit)
 
     segmentation_module.cuda()
 

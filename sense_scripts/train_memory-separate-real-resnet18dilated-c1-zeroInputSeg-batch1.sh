0f1ee072a7293819eff35b65bbd7cc92a349288f
Zhao <alexzhao@cs.utexas.edu>
2019-10-11 14:25:35 -0500

    Initial commit

---
+++ sense_scripts/train_memory-separate-real-resnet18dilated-c1-zeroInputSeg-batch1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+srun --mpi=pmi2 -p $1 -n1 --gres=gpu:8 --ntasks-per-node=1 --cpus-per-task=5 \
+python -u train_memory_separate.py --gpus 0,1,2,3 --cfg config/ade20k-memory-separate-real-resnet18dilated-c1-zeroInputSeg-batch1.yaml \
+2>&1 | tee ./output/output_memory_separate_real_c1_zeroInputSeg_batch1.log

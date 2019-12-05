2032994a39206df50ba0456dcca17379bb3436a0
Zhao <alexzhao@cs.utexas.edu>
2019-12-05 10:35:01 -0600

    Initial commit

--- models/models.py
+++ models/models.py
@@ -493,6 +493,11 @@ class ModelBuilder:
                 num_class=num_class,
                 fc_dim=fc_dim,
                 use_softmax=use_softmax)
+        elif arch == 'c1_aggregation':
+            net_decoder = C1_Aggregation(
+                num_class=num_class,
+                fc_dim=fc_dim,
+                use_softmax=use_softmax)
         elif arch == 'ppm':
             net_decoder = PPM(
                 num_class=num_class,
@@ -968,6 +973,31 @@ class C1(nn.Module):
         self.use_softmax = use_softmax
 
         self.cbr = conv3x3_bn_relu(fc_dim, fc_dim // 4, 1)
+
+        # last conv
+        self.conv_last = nn.Conv2d(fc_dim // 4, num_class, 1, 1, 0)
+
+    def forward(self, conv_out, segSize=None):
+        conv5 = conv_out[-1]
+        x = self.cbr(conv5)
+        x = self.conv_last(x)
+
+        if self.use_softmax: # is True during inference
+            x = nn.functional.interpolate(
+                x, size=segSize, mode='bilinear', align_corners=False)
+            x = nn.functional.softmax(x, dim=1)
+            #x = nn.functional.log_softmax(x, dim=1)
+        else:
+            x = nn.functional.log_softmax(x, dim=1)
+
+        return x
+
+class C1_Aggregation(nn.Module):
+    def __init__(self, num_class=150, fc_dim=2048, use_softmax=False):
+        super(C1_Aggregation, self).__init__()
+        self.use_softmax = use_softmax
+
+        self.cbr = conv3x3_bn_relu(fc_dim//2*3, fc_dim // 4, 1)
 
         # last conv
         self.conv_last = nn.Conv2d(fc_dim // 4, num_class, 1, 1, 0)

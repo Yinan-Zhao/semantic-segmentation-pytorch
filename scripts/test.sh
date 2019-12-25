d99d70423a3942e813ffbe77026cb77a82a33ade
Zhao <alexzhao@cs.utexas.edu>
2019-12-25 06:37:00 -0600

    Initial commit

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-python eval_memory_separate_multipro.py --gpus 0,1,2,3 --cfg $1 --ref_val_start 0 --ref_val_end 1 \
-2>&1 | tee $2.log
+#python eval_memory_separate_multipro.py --gpus 0,1,2,3 --cfg $1 --ref_val_start 0 --ref_val_end 1 \
+#2>&1 | tee $2.log
+python eval_memory_separate_multipro.py --gpus 0,1,2,3 --cfg $1 --ref_val_start 0 --ref_val_end 2 \
+2>&1 | tee $2_ref0-2.log
+python eval_memory_separate_multipro.py --gpus 0,1,2,3 --cfg $1 --ref_val_start 0 --ref_val_end 3 \
+2>&1 | tee $2_ref0-3.log
 python eval_memory_separate_multipro.py --gpus 0,1,2,3 --cfg $1 --ref_val_start 1 --ref_val_end 2 \
 2>&1 | tee $2_ref1.log
 python eval_memory_separate_multipro.py --gpus 0,1,2,3 --cfg $1 --ref_val_start 10 --ref_val_end 11 \
@@ -13,5 +17,7 @@ python eval_memory_separate_multipro.py --gpus 0,1,2,3 --cfg $1 --ref_val_start 
 2>&1 | tee $2_gt.log
 python eval_memory_separate_multipro.py --gpus 0,1,2,3 --cfg $1 --ref_val_start 0 --ref_val_end 1 \
 --debug_with_double_complete_random 2>&1 | tee $2_double_complete_random.log
+python eval_memory_separate_multipro.py --gpus 0,1,2,3 --cfg $1 --ref_val_start 0 --ref_val_end 4 \
+2>&1 | tee $2_ref0-4.log
 
 
